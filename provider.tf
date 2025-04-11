539f9f2ebbab2fcf41cf5c2810270dcf1dd3a5e2
LadyN-N <ukibuh@gmail.com>
2025-04-11 02:21:19 -0400

    update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,20 +2,20 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.89.0"
+      version = "5.5.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  # Configuration options
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "ladyn-terraform-backend1"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-backend-dynamodb"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "ladyn-terraform-backend1"
+#     key            = "terraform.tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "terraform-backend-dynamodb"
+#   }
+# }
